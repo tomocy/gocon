3b196ced3c08d4ca66f13bf9471bb2117748dac6
tomocy <tomocy.dev@gmail.com>
2019-10-27 08:15:50 +0900

    add Container
    Container represen a runtime of a container which implements OCI runtime.

--- cmd/gocon/client/client.go
+++ cmd/gocon/client/client.go
@@ -1,6 +1,11 @@
 package client
 
-import "github.com/urfave/cli"
+import (
+	"os"
+
+	"github.com/opencontainers/runtime-spec/specs-go"
+	"github.com/urfave/cli"
+)
 
 func New() Client {
 	return newClient()
@@ -29,4 +34,13 @@ func (c *client) setUp() {
 func (c *client) setBasic() {
 	c.Name = "gocon"
 	c.Usage = "a CLI client which implements OCI runtime specification and is presented in Go Confenrece'19 Autumn in Tokyo"
+}
+
+type Container interface {
+	State() (*specs.State, error)
+	Clone(args ...string) error
+	Init(*specs.Spec) error
+	Start() error
+	Kill(os.Signal) error
+	Delete() error
 }
