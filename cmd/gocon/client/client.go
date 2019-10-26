ff98f33df0e9dca67d6fe5e985025dd8ba3e454f
tomocy <tomocy.dev@gmail.com>
2019-10-27 07:55:49 +0900

    add newClient
    newClient generates new client which is ready for use.

--- cmd/gocon/client/client.go
+++ cmd/gocon/client/client.go
@@ -2,6 +2,23 @@ package client
 
 import "github.com/urfave/cli"
 
+func newClient() *client {
+	c := new(client)
+	c.setUp()
+
+	return c
+}
+
 type client struct {
 	cli.App
+}
+
+func (c *client) setUp() {
+	c.App = *cli.NewApp()
+	c.setBasic()
+}
+
+func (c *client) setBasic() {
+	c.Name = "gocon"
+	c.Usage = "a CLI client which implements OCI runtime specification and is presented in Go Confenrece'19 Autumn in Tokyo"
 }
