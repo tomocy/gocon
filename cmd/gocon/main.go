628fea0744f2c0c4e2476822ea1dacf77dbf767e
tomocy <tomocy.dev@gmail.com>
2019-10-27 07:59:32 +0900

    update entrypoint of gocon command
    a Client generated by client.New runs in the entrypoint of gocon command.

--- cmd/gocon/main.go
+++ cmd/gocon/main.go
@@ -1,3 +1,16 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+
+	"github.com/tomocy/gocon/cmd/gocon/client"
+)
+
+func main() {
+	c := client.New()
+	if err := c.Run(os.Args); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
