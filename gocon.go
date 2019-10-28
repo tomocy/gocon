7505b661fb2c47158e14a7739460ce4c3e66e2dd
tomocy <tomocy.dev@gmail.com>
2019-10-28 22:56:20 +0900

    add New
    New generates new container of the given id.

--- gocon.go
+++ gocon.go
@@ -2,6 +2,14 @@ package gocon
 
 import "github.com/opencontainers/runtime-spec/specs-go"
 
+func New(id string) *Container {
+	return &Container{
+		state: state{
+			ID: id,
+		},
+	}
+}
+
 type Container struct {
 	state
 }
