6be5612c4a7e44c7767c2cf6a9621431f95a32b9
tomocy <tomocy.dev@gmail.com>
2019-10-28 22:06:21 +0900

    add New
    New generate new Client which is ready for use.

--- cmd/gocon/client/client.go
+++ cmd/gocon/client/client.go
@@ -2,6 +2,23 @@ package client
 
 import "github.com/urfave/cli"
 
+func New() *Client {
+	c := new(Client)
+	c.setUp()
+
+	return c
+}
+
 type Client struct {
 	cli.App
+}
+
+func (c *Client) setUp() {
+	c.App = *cli.NewApp()
+	c.setBasic()
+}
+
+func (c *Client) setBasic() {
+	c.Name = "gocon"
+	c.Usage = "a container runtime which implements OCU runtime specification"
 }
