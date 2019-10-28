9b43ffc326f2792bef3c065253168b817607e3fd
tomocy <tomocy.dev@gmail.com>
2019-10-28 23:41:15 +0900

    add (*Container).Clone
    (*Container).Clone clones the process itself with the given args.

--- gocon_linux.go
+++ gocon_linux.go
@@ -4,8 +4,44 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"syscall"
+
+	"golang.org/x/sys/unix"
 )
+
+func (c *Container) Clone(args ...string) error {
+	cmd := c.buildCloneCmd(args...)
+	if err := cmd.Start(); err != nil {
+		return err
+	}
+
+	c.Pid = cmd.Process.Pid
+	if err := c.save(); err != nil {
+		cmd.Process.Kill()
+		return fmt.Errorf("failed to save: %s", err)
+	}
+
+	return cmd.Wait()
+}
+
+func (c *Container) buildCloneCmd(args ...string) *exec.Cmd {
+	cmd := exec.Command("/proc/self/exe", args...)
+	cmd.SysProcAttr = &unix.SysProcAttr{
+		Cloneflags: unix.CLONE_NEWIPC | unix.CLONE_NEWNET | unix.CLONE_NEWNS |
+			unix.CLONE_NEWPID | unix.CLONE_NEWUSER | unix.CLONE_NEWUTS,
+		UidMappings: []syscall.SysProcIDMap{
+			{ContainerID: 0, HostID: os.Getuid(), Size: 1},
+		},
+		GidMappings: []syscall.SysProcIDMap{
+			{ContainerID: 0, HostID: os.Getgid(), Size: 1},
+		},
+	}
+	cmd.Stdin, cmd.Stdout, cmd.Stderr = os.Stdin, os.Stdout, os.Stderr
+
+	return cmd
+}
 
 func (c *Container) save() error {
 	dst, err := os.OpenFile(c.stateFilename(), os.O_CREATE|os.O_WRONLY, 0744)
